62647047c568e777f24d31ea4ebe7d4eed4df3a2
esmerquenn <esmereyvazov@gmail.com>
2025-01-27 19:09:05 +0400

    responsive intro

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -18,28 +18,30 @@ function Home() {
   return (
     <div className="bg-custom  text-white   pt-20 px-10">
       {/* <h1 className=" uppercase"> Esmer eshraty</h1> */}
-      <div className=" flex   h-full  justify-between">
-        <div className=" w-2/5">
-          <h1 className=" font-fancy uppercase text-[5.7vw] font-bold ">
+      <div className=" flex flex-col lg:flex-row   h-full  justify-between">
+        <div className=" mt-40 sm:mt-28 xl:w-2/5 flex items-end">
+          <h1 className=" font-fancy uppercase text-[2rem] sm:text-[7vw] md:text-[5.7vw] font-bold ">
             <span className="block">
               Web developer <span className=" text-lg text-pink-50">& </span>
             </span>
             <span>mentor</span>
           </h1>
         </div>
-        <div className=" flex flex-col justify-between w-2/5">
-          <div className=" flex items-center justify-start ">
-            <h3 className=" font-fancy   text-[7vw]">{new Date().getDate()}</h3>
-            <div className=" w-[80px]" >
+        <div className=" flex flex-col justify-between xl:w-2/5">
+          <div className=" flex items-center justify-start absolute top-[100px] right-[20%] lg:static ">
+            <h3 className=" font-fancy  text-[5rem]  lg:text-[7vw]">{new Date().getDate()}</h3>
+            <div className=" w-[80px]">
               <h6 className=" font-fancy text-2xl ">{months[new Date().getMonth()]}</h6>
               <span className="font-poppins text-sm">Available for work</span>
             </div>
           </div>
-          <p className=" font-poppins text-2xl  text-right pb-2">
+          <p className=" font-poppins text-lg md:text-2xl  text-right pb-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab iusto tenetur eaque dolores adipisci, at culpa
             voluptatibus obcaecati ducimus neque itaque animi nostrum magnam ullam natus ut beatae accusantium minus iste atque
-            laboriosam architecto, quaerat facilis. Corporis, iusto voluptas modi perspiciatis animi reprehenderit aliquid
-            excepturi voluptatibus unde minus deserunt sapiente, cumque, esse deleniti explicabo! Ipsum?
+            <span className=" hidden md:block">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur adipisci sequi officiis dolorum quaerat vel
+              animi ad fuga ab veniam.
+            </span>
           </p>
         </div>
       </div>
