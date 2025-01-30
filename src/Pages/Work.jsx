9d4ff1ffdae6d2f3d13cb14475e2b94b4396013a
esmerquenn <esmereyvazov@gmail.com>
2025-01-30 10:54:31 +0400

    idont know

--- src/Pages/Work.jsx
+++ src/Pages/Work.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+
 function Work() {
+
   return (
     <div className=" bg-custom-work min-h-screen    pt-28 px-10 flex items-start justify-between">
       <div className=" font-poppins text-white sticky min-h-[50vh] flex justify-end flex-col  p-10 top-40 z-10">
@@ -7,17 +8,20 @@ function Work() {
         <p className=" my-3 text-xl">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur quibusdam incidunt dolorum sint similique cumque
           architecto autem unde ut totam a in dolore labore doloribus sequi nihil saepe aut nulla qui rerum consectetur,
-         
         </p>
         <p className="text-xl">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur quibusdam incidunt dolorum sint similique cumque
-          architecto autem unde ut totam a in dolore ab cumque itaque assumenda perferendis. Obcaecati officia,
-          dolorem ducimus nobis distinctio molestias neque modi.
+          architecto autem unde ut totam a in dolore ab cumque itaque assumenda perferendis. Obcaecati officia, dolorem ducimus
+          nobis distinctio molestias neque modi.
         </p>
       </div>
       <div className=" flex flex-col items-end gap-10 px-[10%] ">
         <div>
-          <h2 className=" uppercase text-white text-[100px] tracking-wide font-bold font-fancy leading-[60px] mb-7">Barshop</h2>
+          {/* <h2 className=" uppercase text-white text-[100px] tracking-wide font-bold font-fancy leading-[60px] transition-all duration-500 mb-7 hover:italic hover:translate-x-3"> */}
+          <h2 className="uppercase text-white text-[100px] tracking-wide font-bold font-fancy leading-[60px] transition-translate duration-500 mb-7 hover:italic hover:translate-x-1">
+            Barshop
+          </h2>
+
           <span className="text-white uppercase font-medium font-poppins text-lg italic">- Front-end developer </span>
         </div>
         <div>
