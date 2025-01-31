bc93ee91d4cfd6f441207e0c1a876eb603b55783
esmerquenn <esmereyvazov@gmail.com>
2025-01-31 21:06:28 +0400

    a

--- src/Components/CvButton.jsx
+++ src/Components/CvButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import cvFile from "./../assets/eshraty_esmer.pdf";
 function CvButton() {
   return (
-    <button className="  capitalize text-lg font-medium w-[200px] h-[200px] border  border-white rounded-full flex items-center justify-center">
+    <button className="  capitalize text-lg md:text-2xl font-poppins font-medium w-[200px] h-[200px] border  border-white rounded-full flex items-center justify-center">
       <a href={cvFile} download="eshraty_esmer.pdf">
         <button className="cv-button">Download CV</button>
       </a>
