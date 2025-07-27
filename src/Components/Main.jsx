70ad7635d4c12728e8f3d7e9162c5c7c187e322c
esmerquenn <esmereyvazov@gmail.com>
2025-07-27 10:27:35 +0400

    main deleted comments

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import esmer from "./../assets/mee.jpg";
-import bah from "./../assets/bah.png";
-import bar from "./../assets/bar.png";
-import my from "./../assets/my.png";
-import workr from "./../assets/workr.png";
 import WorksMain from "./WorksMain";
 function Main() {
   return (
@@ -37,40 +33,6 @@ function Main() {
           <img className="w-full opacity-50 border-4 border-white" src={esmer} alt="esmer" />
         </div>
       </div>
-      {/* <div className=" py-20 ">
-        <h3 className="px-10 font-fancy uppercase text-3xl md:text-5xl mb-10">Featured work</h3>
-        <div className="my-grid px-10 grid grid-cols-3 gap-10">
-          <a target="_blank" href="https://baharatci-clone.vercel.app/" className=" flex items-center gap-2">
-            <span className="hidden md:block uppercase  text-right">
-              <h3 className="font-fancy text-2xl md:text-3xl font-bold">Baharatci.az</h3>
-              <h6 className=" font-poppins text-sm">html/css/js</h6>
-            </span>
-            <img className="w-[340px] opacity-50 border-4 border-white " src={bah} alt="baharatci" />
-          </a>
-          <a target="_blank" href="https://barshop-clone.vercel.app/" className=" flex items-center flex-row-reverse gap-2">
-            <span className=" hidden md:block uppercase  text-left">
-              <h3 className="font-fancy text-2xl md:text-3xl  font-bold">Barshop.az</h3>
-              <h6 className=" font-poppins text-sm">html/tailwind/js</h6>
-            </span>
-            <img className="w-[340px] opacity-50 border-4 border-white " src={bar} alt="barshop" />
-          </a>
-          <a target="_blank" href="https://my-insure.vercel.app/" className=" flex items-center gap-2">
-            <span className="hidden md:block uppercase  text-right">
-              <h3 className="font-fancy text-2xl md:text-3xl font-bold">myinsure.az</h3>
-              <h6 className=" font-poppins text-sm">html/css/alpine.js/js</h6>
-            </span>
-            <img className="w-[340px] opacity-50 border-4 border-white " src={my} alt="1sigorta.az" />
-          </a>
-          <a target="_blank" href="https://barshop-clone.vercel.app/" className=" flex items-center flex-row-reverse gap-2">
-            <span className=" hidden md:block uppercase  text-left">
-              <h3 className="font-fancy text-2xl md:text-3xl  font-bold">workr.az</h3>
-              <h6 className=" font-poppins text-sm">html/tailwind/js</h6>
-            </span>
-            <img className="w-[340px] opacity-50 border-4 border-white " src={workr} alt="workr" />
-          </a>
-        </div>
-      </div> */}
-
       <WorksMain />
       <div className="pb-20 flex justify-center items-center ">
         <button className=" transition-button bg-white text-black px-4 py-2  rounded-2xl ">
